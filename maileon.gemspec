b3bcf55961def2b2c03375067f357c76e1b2ff3c
Ain Tohvri <at@interactive-pioneers.de>
2015-02-23 13:42:56 +0100

    Add initilisation test with API key

--- maileon.gemspec
+++ maileon.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "excon", "~> 0.39"
   spec.add_development_dependency "bundler", "~> 1.6"
+  spec.add_development_dependency "dotenv-rails", "~> 1.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.2"
   spec.add_development_dependency "rspec-nc", "~> 0.2"
--- spec/lib/api_spec.rb
+++ spec/lib/api_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'dotenv'
 
 describe Maileon::API do
 
@@ -8,6 +9,16 @@ describe Maileon::API do
 
   it 'should initilize with API key' do
     expect(Maileon::API.new('2iaodsfi4u83943uruqf')).to be_an_instance_of Maileon::API
+  end
+
+  it 'should have API environment variable set' do
+    Dotenv.load
+    expect(ENV['MAILEON_APIKEY']).to be_a(String)
+  end
+
+  it 'should initilize with API key from environment' do
+    Dotenv.load
+    expect(Maileon::API.new).to be_an_instance_of Maileon::API
   end
 
 end
