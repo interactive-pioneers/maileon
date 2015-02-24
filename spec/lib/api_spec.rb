a15ea19271615ef29cbfa8bc3514f7eb380763fd
Ain Tohvri <at@interactive-pioneers.de>
2015-02-24 11:49:08 +0100

    Update to context-based RSpec

--- spec/lib/api_spec.rb
+++ spec/lib/api_spec.rb
@@ -4,89 +4,59 @@ require 'excon'
 
 describe Maileon::API do
 
-  describe "initilisation" do
-
-    it 'should throw error without API key' do
-      expect { Maileon::API.new }.to raise_error
+  describe '.initialize' do
+    context 'without API key' do
+      it { expect { Maileon::API.new }.to raise_error }
     end
-
-    it 'should initilize with API key' do
-      expect(Maileon::API.new('2iaodsfi4u83943uruqf')).to be_an_instance_of Maileon::API
+    context 'with API key' do
+      it { expect(Maileon::API.new('2iaodsfi4u83943uruqf')).to be_an_instance_of Maileon::API }
     end
-
-    it 'should have API environment variable set' do
-      Dotenv.load
-      expect(ENV['MAILEON_APIKEY']).to be_a(String)
-    end
-
-    it 'should initilize with API key from environment' do
-      Dotenv.load
-      expect(Maileon::API.new).to be_an_instance_of Maileon::API
+    context 'with environment-based API key' do
+      it {
+        ENV['MAILEON_APIKEY'] = 'adsfadsi4292r0vajsfdafldkaf'
+        is_expected.to be_an_instance_of Maileon::API
+      }
     end
   end
 
-  describe "attributes" do
+  describe Maileon::API.new('asdfasdfasdfa') do
+    context 'when initiliased' do
+      it { is_expected.to have_attributes(:host => a_string_starting_with("https://")) }
+      it { is_expected.to have_attributes(:path => a_string_starting_with("/")) }
+      it { is_expected.to have_attributes(:apikey => Base64.encode64('asdfasdfasdfa').strip) }
+      it { is_expected.to have_attributes(:debug => false) }
+      it { is_expected.to have_attributes(:session => be) }
+    end
+  end
 
+  describe 'ping' do
     before(:all) do
-      Dotenv.load
+      skip "Not implemented"
       @maileon = Maileon::API.new
     end
-
-    it 'should have host' do
-      expect(@maileon).to have_attributes(:host => a_string_starting_with("https"))
+    it 'should respond with 200' do
+      # TODO implement
     end
-
-    it 'should have path' do
-      expect(@maileon).to have_attributes(:path => a_string_starting_with("/"))
-    end
-
-    it 'should have apikey' do
-      expect(@maileon).to have_attributes(:apikey => Base64.encode64(ENV['MAILEON_APIKEY']).strip)
-    end
-
-    it 'should have debug' do
-      expect(@maileon).to have_attributes(:debug => false)
-    end
-
-    it 'should have session' do
-      expect(@maileon).to have_attributes(:session => be)
-    end
-
   end
 
-  describe "create contact" do
-
+  describe 'subscription' do
     before(:all) do
       @maileon = Maileon::API.new
     end
-
-    it 'should fail without parameters' do
-      expect { @maileon.create_contact() }.to raise_error(ArgumentError)
+    context 'without parameters' do
+      it { expect { @maileon.create_contact() }.to raise_error(ArgumentError) }
     end
-
-    it 'should fail with empty parameters' do
-      expect { @maileon.create_contact({}) }.to raise_error(ArgumentError, "No parameters.")
+    context 'with empty parameters' do
+      it { expect { @maileon.create_contact({}) }.to raise_error(ArgumentError, "No parameters.") }
     end
-
-    it 'should fail without email' do
-      expect { @maileon.create_contact({:permission => 1}) }.to raise_error(ArgumentError, "Email is mandatory to create contact.")
+    context 'without email' do
+      it { expect { @maileon.create_contact({:permission => 1}) }.to raise_error(ArgumentError, "Email is mandatory to create contact.") }
     end
-
-    it 'should throw validation error on invalid email' do
-      params = {
-        :email => 'dummy@email'
-      }
-      expect { @maileon.create_contact(params) }.to raise_error(Maileon::Errors::ValidationError, "Invalid email format.")
+    context 'with invalid email' do
+      it { expect { @maileon.create_contact({ :email => 'dummy@email' }) }.to raise_error(Maileon::Errors::ValidationError, "Invalid email format.") }
     end
-
-    it 'should not throw validation error on valid email' do
-      params = {
-        :email => 'dummy@email.com'
-      }
-      expect { @maileon.create_contact(params) }.not_to raise_error
+    context 'with valid email' do
+     it { expect { @maileon.create_contact({ :email => 'dummy@email.com' }) }.not_to raise_error }
     end
-
   end
-
-
 end
